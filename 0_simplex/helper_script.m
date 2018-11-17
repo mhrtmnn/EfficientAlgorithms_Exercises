72582c020d9e757f6f84a58cce4de9afef4c93b7
Marco Hartmann <marcohartmann95@gmail.com>
2018-11-17 10:57:01 +0100

    Cleanup helper script

--- 0_simplex/helper_script.m
+++ 0_simplex/helper_script.m
@@ -12,15 +12,11 @@ while 1
 	[x_mat,fval,exitflag,output] = linprog(c_start, -eye(n+m), zeros(n+m,1), A_start, b);
 	if ~exitflag || norm(fval) > 0.1
 		continue
-	else
-		disp('Found sensible start corner problem!')
 	end
 
 	[x_mat,fval,exitflag,output] = linprog(c, -eye(n), zeros(n,1), A, b);
 	if ~exitflag
 		continue
-	else
-		disp('Found sensible problem!')
 	end
 
 	x_own = simplex(c, A, b);
@@ -28,8 +24,7 @@ while 1
 	x_own_enum = corner_enumeration_solver(c, A, b);
 
 	if (norm(x_own - x_mat) < 10e-8) && (norm(x_own_enum - x_mat) < 10e-8)
-		disp('================== Seems about right ==================');
-
+		disp('================== Solution appears to be correct ==================');
 	else
 		error('Seems like an error')
 	end
